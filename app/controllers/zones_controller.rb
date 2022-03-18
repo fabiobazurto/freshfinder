4732810e9146f92b1c298160704133b4a167a08f
Fabio Bazurto Blacio <fabio.bazurto@gmail.com>
2022-03-18 02:59:02 -0500

    better ux

--- app/controllers/zones_controller.rb
+++ app/controllers/zones_controller.rb
@@ -1,21 +1,25 @@
 class ZonesController < ApplicationController
 
+  before_action :new_zone, only: [:index,:create]
+  
   def index
-    @zone = Zone.new
   end
 
   def create
-    @zone = Zone.new
-    p params[:address]
-    @result = Zone.search(address: params[:address])  if params[:address].present?
+    @result = Zone.ready_to_delivery?(address: params[:address])  if params[:address].present?
     respond_to do |format|
         format.html { render :search }
     end
-
   end
 
   def zone_params
     params.fetch(:zone, {}).permit(:address)
   end
 
+  private
+
+  def new_zone
+    @zone = Zone.new    
+  end
+  
 end
