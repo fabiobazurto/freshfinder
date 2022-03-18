738830d3fd5d51a5fa880719fb89ce07ccdd1985
Fabio Bazurto Blacio <fabio.bazurto@gmail.com>
2022-03-17 23:18:16 -0500

    removing comment

--- db/migrate/20220318005348_add_coords_to_zones.rb
+++ db/migrate/20220318005348_add_coords_to_zones.rb
@@ -1,6 +1,5 @@
 class AddCoordsToZones < ActiveRecord::Migration[6.1]
   def change
-#    add_column :zones, :coords, :float, array: true, default: []
     add_column :zones,:coords, :geometry
     add_index  :zones, :coords, using: :gist
   end
